00992ebc385b014a5a275eb0b7f494053c9983a1
Luis Bizarro <luis@activetheory.net>
2021-04-18 14:29:01 -0300

    Intregration of Prismic API with Express.

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  })
+}
+
+const handleLinkResolver = doc => {
+  // // Define the url depending on the document type
+  // if (doc.type === 'page') {
+  //   return '/page/' + doc.uid;
+  // } else if (doc.type === 'blog_post') {
+  //   return '/blog/' + doc.uid;
+  // }
+
+  // Default to homepage
+  return '/'
+}
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver
+  }
+
+  res.locals.PrismicDOM = PrismicDOM
+
+  next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -11,7 +46,17 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/about', async (req, res) => {
-  res.render('pages/about')
+  initApi(req).then(api => {
+    api.query(Prismic.Predicates.any('document.type', ['about', 'meta'])).then(response => {
+      const { results } = response
+      const [about, meta] = results
+
+      res.render('pages/about', {
+        about,
+        meta
+      })
+    })
+  })
 })
 
 app.get('/collection', (req, res) => {
